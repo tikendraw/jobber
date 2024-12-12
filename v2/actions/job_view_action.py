8b8f7c3e8d7dc99de2b3d21c9a488479cb62f264
tikendraw <tikendraksahu1029@gmail.com>
2024-12-12 14:39:19 +0530

    added func: job_view_checkout

---
+++ v2/actions/job_view_action.py
@@ -0,0 +1,21 @@
+
+from playwright.async_api import Page
+from logging import getLogger
+from v2.context import browser_action
+from typing import List, Dict
+from v2.items.job_view import job_view_page_action
+
+logger = getLogger(__name__)
+
+
+@browser_action
+async def job_view_checkout(page: Page, urls: List[str]) -> Dict[str, str]:
+    content = {}
+    for url in urls:
+        try:
+            await page.goto(url, wait_until='domcontentloaded')
+            await job_view_page_action(page=page)
+            content[page.url] = await page.content()
+        except Exception as e:
+            logger.error(f"Error processing URL {url}: {e}")
+    return content
