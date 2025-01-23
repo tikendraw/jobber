33bf0e9ca92abec59c4280304f67436f2f6ca3b3
tikendraw <tikendraksahu1029@gmail.com>
2025-01-23 21:46:24 +0530

    Refactor ScraperEngine: Enhance scraping functionality with configurable parameters for concurrency, depth, retries, and resource blocking. Improved error handling and logging during URL processing and data extraction. Updated method signatures for better clarity and flexibility.

--- v2/scraper/scraper_engine.py
+++ v2/scraper/scraper_engine.py
@@ -1,7 +1,8 @@
 # scraper/scraper_engine.py
 from asyncio import Semaphore, gather
 from pathlib import Path
-from typing import Dict, List
+from pickle import FALSE
+from typing import Dict, List, Optional, Set
 
 from playwright.async_api import BrowserContext, Page, Request, Route, async_playwright
 
@@ -12,29 +13,47 @@ from v2.scraper.scraper_utils import read_cookies, save_cookies
 
 logger = get_logger(__name__)
 
+DEFAULT_MAX_CONCURRENT = 5
+DEFAULT_MAX_DEPTH = 0
+DEFAULT_MAX_RETRIES = 2
+DEFAULT_BLOCKED_RESOURCES = {
+    "image", "media", 
+    "font", 
+    # "stylesheet","other", "manifest", "texttrack"
+}
 
-            
+
 class ScraperEngine:
-    def __init__(self, platform: WebsitePlatform, max_concurrent: int = 5) -> None:
+
+    def __init__(self, platform: WebsitePlatform) -> None:
         """Initialise the scrapper engine"""
-        self.max_concurrent = max_concurrent
         self.platform = platform
 
     def set_semaphore(self, max_concurrent: int = 5) -> None:
         self.semaphore = Semaphore(max_concurrent)
 
-    async def scrap(self, urls: List[str] = None, search_params: Dict = None, credentials: Dict = None,
-                    filters: Dict = None,cookie_file:str|Path=None, *args, **kwargs) -> List[PageResponse]:
+    async def scrap(
+        self,
+        urls: Optional[List[str]] = None,
+        search_params: Optional[Dict] = None,
+        credentials: Optional[Dict] = None,
+        filters: Optional[Dict] = None,
+        cookie_file: Optional[str | Path] = None,
+        headless: bool = False,
+        max_concurrent: Optional[int] = DEFAULT_MAX_CONCURRENT,
+        max_depth: Optional[int] = DEFAULT_MAX_DEPTH,
+        max_retries: Optional[int] = DEFAULT_MAX_RETRIES,
+        blocked_resources: Optional[Set[str]] = DEFAULT_BLOCKED_RESOURCES,
+        *args,  # Consider adding typing if the purpose is known
+        **kwargs,    ) -> List[PageResponse]:
         """Main method to scrap the website"""
-
+        if urls is None and search_params is None:
+            raise ValueError("Provide urls or search params")
+        
         results = []
-        cookie_file = cookie_file or  f'{self.platform.name}-cookies.jsonl'
-        block_media = kwargs.pop("block_media", True)
-        headless = kwargs.pop("headless", True)
-        logger.info("Browser lanching headless: {headless}")
-        max_concurrent = kwargs.pop("max_concurrent", self.max_concurrent)
-        max_depth = kwargs.pop('max_depth', 0)
-
+        cookie_file = cookie_file or f"{self.platform.name}-cookies.jsonl"
+        logger.info(f"Browser launching headless: {headless}")
+        
         self.set_semaphore(max_concurrent)
         try:
             async with async_playwright() as p:
@@ -47,114 +66,139 @@ class ScraperEngine:
 
                 if credentials:
                     login_page = await context.new_page()
-                    await login_page.goto(self.platform.login_url, wait_until='commit')
+                    await login_page.goto(self.platform.login_url, wait_until="commit")
                     try:
-                        await self.platform.login(page=login_page, credentials=credentials)
+                        await self.platform.login(
+                            page=login_page, credentials=credentials
+                        )
                     except Exception as e:
                         logger.error(f"Error while logging in: {e}", exc_info=True)
                     await login_page.close()
 
                 page = await context.new_page()
 
-                if block_media:
-                    await page.route("**/**", self._block_resources)
+                if blocked_resources:
+                        await page.route("**/**", lambda route, request: self._block_resources(route, request, blocked_resources))
 
                 if search_params:
                     try:
-                        await self.platform.search_action(page, search_params=search_params)
+                        await self.platform.search_action(
+                            page, search_params=search_params
+                        )
                     except Exception as e:
-                        logger.error(f"Error while in search action: {e}", exc_info=True)
+                        logger.error(
+                            f"Error while in search action: {e}", exc_info=True
+                        )
 
                 if filters:
                     try:
                         await self.platform.apply_filters(page, filters=filters)
                     except Exception as e:
-                        logger.error(f"Error while applying filters: {e}", exc_info=True)
-                
+                        logger.error(
+                            f"Error while applying filters: {e}", exc_info=True
+                        )
+
                 if urls:
-                    tasks = [self._process_url_with_semaphore(context, url) for url in urls]
+                    tasks = [
+                        self._process_url_with_semaphore(
+                            context, url, max_retries=max_retries
+                        )
+                        for url in urls
+                    ]
                     results = await gather(*tasks)
-                    results = [item for sublist in results if sublist for item in sublist] # flatten the list
-                    
+                    results = [
+                        item for sublist in results if sublist for item in sublist
+                    ]  # flatten the list
+
                 else:
                     try:
-                        results = await self.platform.after_search_action(page=page,max_depth=max_depth, **kwargs)
+                        results = await self.platform.after_search_action(
+                            page=page, max_depth=max_depth, **kwargs
+                        )
                     except Exception as e:
-                        logger.error(f"Error while after search action: {e}", exc_info=True)
-        
-        
-                logger.debug(f'Extracting data...{len(results)}')
+                        logger.error(
+                            f"Error while after search action: {e}", exc_info=True
+                        )
+
+                logger.debug(f"Extracting data...{len(results)}")
                 results = await self._extract_data(results)
-                logger.debug(f'Extracted data...{len(results)}')
+                logger.debug(f"Extracted data...{len(results)}")
 
                 cookie_content = await context.cookies()
                 await save_cookies(content=cookie_content, cookie_file=cookie_file)
                 await page.close()
                 await context.close()
                 await browser.close()
-        finally:            
+        finally:
             return results
-    
-    async def _process_url_with_semaphore(self, context: BrowserContext, url: str) -> List[PageResponse]:
+
+    async def _process_url_with_semaphore(
+        self, context: BrowserContext, url: str, max_retries: int
+    ) -> List[PageResponse]:
         """
         Processes a single URL, navigates to it, and then extracts data. Uses semaphore.
-         """
+        """
         async with self.semaphore:
-          page = await context.new_page()
-          results = await self._process_url(page, url)
-          await page.close()
-          return results
+            page = await context.new_page()
+            results = await self._process_url(page, url, max_retries)
+            await page.close()
+            return results
 
-    
-    async def _process_url(self, page:Page, url: str) -> List[PageResponse]:
-        """
-        Processes a single URL, navigates to it and then extracts data using the correct page object.
-
-        Args:
-            page (Page): The Playwright page object.
-            url (str): The URL to process.
-
-        Returns:
-            List[PageResponse]: A list of PageResponse objects for the processed URL
-        """
+    async def _process_url(
+        self, page: Page, url: str, max_retries: int = 3
+    ) -> List[PageResponse]:
         results = []
-        try:
-            await page.reload()
-            await page.goto(url, wait_until='domcontentloaded')
-           
-            page_obj = self.platform.get_page_object_from_url(url)
-            if page_obj:
-                await page_obj.page_action(page)
+        for attempt in range(max_retries):
+            try:
+                await page.reload()
+                await page.goto(url, wait_until="domcontentloaded")
 
-            page_res = await parse_page_response(page)
-            results.append(page_res)
+                page_obj = self.platform.get_page_object_from_url(url)
+                if page_obj:
+                    await page_obj.page_action(page)
 
-        except Exception as e:
-            logger.error(f"Error processing URL {url}: {e}", exc_info=True)
+                page_res = await parse_page_response(page)
+                results.append(page_res)
+                break  # Exit retry loop on success
+            except Exception as e:
+                logger.error(
+                    f"Attempt {attempt + 1}: Error processing URL {url}: {e}",
+                    exc_info=True,
+                )
+                if attempt == max_retries - 1:
+                    logger.error(f"Max retries reached for URL {url}")
         return results
 
     # TODO: make this better
-    async def _block_resources(self, route:Route, request:Request) -> None:
-        if request.resource_type in {"image", "media"}:
+    async def _block_resources(self, route: Route, request: Request, blocked_resources: Set[str]) -> None:
+        if request.resource_type in blocked_resources:
             await route.abort()
         else:
             await route.continue_()
-
-    async def _extract_data(self, page_responses: List[PageResponse]) -> List[PageResponse]:
+            
+    async def _extract_data(
+        self, page_responses: List[PageResponse]
+    ) -> List[PageResponse]:
         """Extracts the data by the page strategy using concurrent tasks"""
+
         async def extract_single(page_response: PageResponse) -> PageResponse:
-            logger.debug(f'Extracting data for {page_response.url}')
+            logger.debug(f"Extracting data for {page_response.url}")
             page_obj = self.platform.get_page_object_from_url(page_response.url)
             if page_obj and page_obj.extraction_strategy:
                 try:
-                    extracted_response = await page_obj.extraction_strategy.aextract(page_response)
+                    extracted_response = await page_obj.extraction_strategy.aextract(
+                        page_response
+                    )
                     if extracted_response:
-                        logger.debug(f'Extracted data for {page_response.url}')
+                        logger.debug(f"Extracted data for {page_response.url}")
                         return extracted_response
                 except Exception as e:
-                    logger.error(f"Error while extraction for {page_response.url}: {e}", exc_info=True)
+                    logger.error(
+                        f"Error while extraction for {page_response.url}: {e}",
+                        exc_info=True,
+                    )
             else:
-                logger.debug(f'No extraction strategy for {page_response.url}')
+                logger.debug(f"No extraction strategy for {page_response.url}")
                 return page_response
 
         extraction_tasks = [extract_single(response) for response in page_responses]
